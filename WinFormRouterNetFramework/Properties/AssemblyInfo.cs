60635a779ce5713d6d487b15d850acd604f486ed
José Miguel <clasiccode@gmail.com>
2023-05-22 15:12:41 +0200

    Update AssemblyInfo.cs

--- WinFormRouterNetFramework/Properties/AssemblyInfo.cs
+++ WinFormRouterNetFramework/Properties/AssemblyInfo.cs
@@ -5,19 +5,19 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("WinFormRouter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("WinForm Router .NetFramework")]
+[assembly: AssemblyDescription("This router allows you to create an advanced winforms window management")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Codevia")]
-[assembly: AssemblyProduct("WinFormRouter")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyProduct("WinFormRouter for NetFramework")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
+[assembly: AssemblyTrademark("CodeviaDevsTools")]
 [assembly: AssemblyCulture("")]
 
 // Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
 // para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
 // COM, establezca el atributo ComVisible en true en este tipo.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
 [assembly: Guid("2399baca-eede-4d5a-b789-b9c117a6ac00")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]

12e5fc36a22a96c2ac993eb9aa2a827b4e579906
José Miguel <clasiccode@gmail.com>
2025-06-16 10:06:41 +0200

    Update routes model

--- Codevia.Router.Net/Route.cs
+++ Codevia.Router.Net/Route.cs
@@ -12,15 +12,15 @@ namespace Codevia.Router.Net
     /// <typeparam name="T">Type generic of component</typeparam>
     public class Route
     {
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public string Title { get; set; } = "";
         public bool ShowInMenu { get; set; } = true;
         public object Permisions { get; set; }
         public string Description { get; set; } = "";
-        public Image Image { get; set; } = null;
-        public object Type { get; set; } = NavigationType.Navigation;
-        public Form Component { get; set; }
-        public Routes Childrend { get; set; } = null;
+        public Image? Image { get; set; }
+        public NavigationType Type { get; set; } = NavigationType.Navigation;
+        public required Form Component { get; set; }
+        public Routes? Childrend { get; set; }
     }
 
     public class Routes : List<Route> { }
